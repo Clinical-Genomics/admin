51397e70c8902a66efd2925f396aadaefc3856e0
Robin Andeer <robin.andeer@gmail.com>
2017-03-31 12:08:47 +0200

    add submit from project view

--- cgadmin/server/forms.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from wtforms_alchemy import ModelForm
-
-from cgadmin.store.models import Project
-
-
-class ProjectForm(ModelForm):
-    class Meta:
-        model = Project
