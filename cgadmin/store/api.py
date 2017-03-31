847a80a834db3b083208988e289418b39068930a
Robin Andeer <robin.andeer@gmail.com>
2017-03-31 11:12:04 +0200

    move stuff into API class

--- cgadmin/store/api.py
+++ cgadmin/store/api.py
@@ -5,72 +5,26 @@ from .models import Model, ApplicationTagVersion, ApplicationTag
 from cgadmin.schema import schema_project
 
 
-def connect(db_uri):
-    """Connect to database."""
-    db = SQLClient({'SQL_DATABASE_URI': db_uri}, model_class=Model)
-    return db
+class AdminDatabase(SQLClient):
+    """docstring for AdminDatabase"""
+    def __init__(self, db_uri):
+        super(AdminDatabase, self).__init__({'SQL_DATABASE_URI': db_uri}, model_class=Model)
 
+    def latest_version(self, apptag_id):
+        """Get the latest version of an application tag."""
+        version = (self.ApplicationTagVersion
+                       .join(ApplicationTagVersion.apptag)
+                       .filter(ApplicationTag.name == apptag_id)
+                       .order_by(ApplicationTagVersion.valid_from.desc())
+                       .first())
+        return version
 
-def connect_app(app):
-    """Connect Flask application."""
-    db = connect(app.config['SQL_DATABASE_URI'])
-    return db
-
-
-def latest_version(db, apptag_id):
-    """Get the latest version of an application tag."""
-    version = (db.ApplicationTagVersion.join(ApplicationTagVersion.apptag)
-                 .filter(ApplicationTag.name == apptag_id)
-                 .order_by(ApplicationTagVersion.valid_from.desc())
-                 .first())
-    return version
-
-
-def parse_db_project(new_project):
-    """Parse Project from database to JSON."""
-    project_data = {
-        'name': new_project.name,
-        'customer': new_project.customer.customer_id,
-        'families': [],
-    }
-    for family in new_project.families:
-        family_data = {
-            'name': family.name,
-            'panels': family.panels,
-            'priority': family.priority,
-            'delivery_type': family.delivery_type,
-            'require_qcok': family.require_qcok,
-            'samples': [],
+    def full_schema(self):
+        """Fill out schema with all possible values."""
+        schema_project['properties']['customer'] = {
+            "enum": [customer.customer_id for customer in self.Customer]
         }
-        for sample in family.samples:
-            sample_data = {
-                'name': sample.name,
-                'sex': sample.sex,
-                'status': sample.status,
-                'application_tag': sample.application_tag.name,
-                'capture_kit': sample.capture_kit,
-                'source': sample.source,
-                'container': sample.container,
-                'container_name': sample.container_name,
-                'well_position': sample.well_position,
-                'quantity': sample.quantity,
-            }
-            for parent_id in ('father', 'mother'):
-                parent_sample = getattr(sample, parent_id)
-                if parent_sample:
-                    sample_data[parent_id] = parent_sample.name
-            family_data['samples'].append(sample_data)
-        project_data['families'].append(family_data)
-
-    return project_data
-
-
-def full_schema(admin_db):
-    """Fill out schema with all possible values."""
-    schema_project['properties']['customer'] = {
-        "enum": [customer.customer_id for customer in admin_db.Customer]
-    }
-    schema_project['properties']['families']['items']['properties']['samples']['items']['properties']['application_tag'] = {
-        "enum": [apptag.name for apptag in admin_db.ApplicationTag]
-    }
-    return schema_project
+        schema_project['properties']['families']['items']['properties']['samples']['items']['properties']['application_tag'] = {
+            "enum": [apptag.name for apptag in self.ApplicationTag]
+        }
+        return schema_project
